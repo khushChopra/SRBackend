0df360b0934bccc70bff844e3bfa57d16cecd486
Khush <khush.chopra.cse16@itbhu.ac.in>
2018-10-22 15:15:22 +0530

    trying conn

--- index.php
+++ index.php
@@ -2,14 +2,6 @@
 
 echo "Helloa Khush!";
 
-try {
-    $conn = new PDO("sqlsrv:server = tcp:searchrescue.database.windows.net,1433; Database = searchrescue", "piyushchoudhary", "piyush@123");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo $conn;
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+
 
 ?>
